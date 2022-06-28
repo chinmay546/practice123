5f0b50dc6171a596e8b2aa1c69496cc35276f8d3
chinmay_coditas <chinmay.garge@coditas.com>
2022-06-28 22:34:29 +0530

    assignment -1

---
+++ assinment/assign_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    printf("Assinment 1 ");
+  
+    return 0;
+}
